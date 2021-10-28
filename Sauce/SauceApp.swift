46e4773812925955ed68bbf087e12982a431de1b
Jared Grimes <jaredlgrimes@hotmail.com>
2021-10-27 20:53:45 -0400

    Initial Commit

---
+++ Sauce/SauceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SauceApp.swift
+//  Sauce
+//
+//  Created by Jared Grimes on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct SauceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
