a420a30152663680f32076f277a735be5775be9c
Jared Grimes <jaredlgrimes@hotmail.com>
2021-10-31 11:26:43 -0400

    Added search

--- Sauce/Controllers/PlaceSearchController.swift
+++ Sauce/Controllers/PlaceSearchController.swift
@@ -20,7 +20,9 @@ struct PlaceSearchController: View {
         }
     }
     
-    private var localSearch: MKLocalSearch? {
+    @State var queryString: String = ""
+    
+    @State var localSearch: MKLocalSearch? {
         willSet {
             // Clear the results and cancel the currently running local search before starting a new search.
             places = nil
@@ -28,22 +30,29 @@ struct PlaceSearchController: View {
         }
     }
     
-    private mutating func search(using searchRequest: MKLocalSearch.Request) {
+    /// - Parameter queryString: A search string from the text the user entered into `UISearchBar`
+    func search(queryString: String) -> Void {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = queryString
+        search(using: searchRequest)
+    }
+
+    func search(using searchRequest: MKLocalSearch.Request) {
         // Confine the map search area to an area around the user's current location.
         searchRequest.region = boundingRegion
-        
+
         // Include only point of interest results. This excludes results based on address matches.
         searchRequest.resultTypes = .pointOfInterest
-        
+
         localSearch = MKLocalSearch(request: searchRequest)
         localSearch?.start { [self] (response, error) in
             guard error == nil else {
                 // self.displaySearchError(error)
                 return
             }
-            
+
             self.places = response?.mapItems
-            
+
             // Used when setting the map's region in `prepareForSegue`.
             if let updatedRegion = response?.boundingRegion {
                 self.boundingRegion = updatedRegion
@@ -52,7 +61,7 @@ struct PlaceSearchController: View {
     }
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        PlaceSearchView(onChange: self.search, places: $places)
     }
 }
 
--- Sauce/Views/AddPlace/PlaceSearchView.swift
+++ Sauce/Views/AddPlace/PlaceSearchView.swift
@@ -6,19 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct PlaceSearch: View {
-    @Binding var searchItems: Array<String>
-    @Binding var value: String
-    @Binding var onChange: () -> String
+func void(str: String) -> Void {}
+
+struct PlaceSearchView: View {
+    @State var value: String = ""
+    var onChange: (String) -> Void
+    @Binding var places: [MKMapItem]?
+    
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField(
+                "Search for a place",
+                text: $value
+            ).onChange(of: value, perform: { newValue in
+                onChange(newValue)
+            })
+            List {
+                ForEach(places ?? [], id: \.self) { place in
+                    VStack(alignment: .leading) {
+                        Text(place.name!)
+                        Text(place.placemark.formattedAddress ?? "").font(.caption)
+                    }
+                }
+            }
+        }
     }
 }
 
-//struct PlaceSearch_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PlaceSearch()
-//    }
-//}
+struct PlaceSearch_Previews: PreviewProvider {
+    static var previews: some View {
+        PlaceSearchView(onChange: void, places: .constant([]))
+    }
+}
